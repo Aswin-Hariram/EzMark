e4192e433dea1a64e81de670c33130831005e739
Aswinhariram-skct <aswincseskct@gmail.com>
2024-12-23 14:34:11 +0530

    admindashboard

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import HomeScreen from './Screens/HomeScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from './Screens/LoginScreen';
 import { HeaderTitle } from '@react-navigation/elements';
+import AdminDashboardScreen from './Screens/AdminDashboardScreen';
 export default function App() {
 
 
@@ -11,6 +12,7 @@ export default function App() {
   function RootStack() {
     return (
       <Stack.Navigator screenOptions={{headerShown:false}}>
+       <Stack.Screen name="AdminDashboardScreen" component={AdminDashboardScreen}/>
         <Stack.Screen name="Login" component={LoginScreen}/>
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
