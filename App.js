c9c7817c327db401dcc94595c7608a0ece6a3986
Aswinhariram-skct <aswincseskct@gmail.com>
2024-12-23 11:35:59 +0530

    navigation added

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './Screens/HomeScreen';
-
+import { NavigationContainer } from '@react-navigation/native';
 export default function App() {
 
 
-  const RootStack = createNativeStackNavigator({
-    screens: {
-      Home: HomeScreen,
-    },
-  });
+  const Stack = createNativeStackNavigator();
+
+  function RootStack() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    );
+  }
   return (
-    <RootStack />
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
   );
 }
 
