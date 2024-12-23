6856f67e1588b3000ac71832c92118c1749831ce
Aswinhariram-skct <aswincseskct@gmail.com>
2024-12-23 10:28:18 +0530

    navigation setup completed

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './Screens/HomeScreen';
 
 export default function App() {
+
+
+  const RootStack = createNativeStackNavigator({
+    screens: {
+      Home: HomeScreen,
+    },
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RootStack />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
