6bfcb0eca5e57c63631d6480b95ed6cdb180f5bb
Aswinhariram-skct <aswincseskct@gmail.com>
2025-01-04 23:26:58 +0530

    status bar update

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -22,6 +22,7 @@ import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import LottieView from 'lottie-react-native';
+import { StatusBar } from 'expo-status-bar';
 
 const LoginScreen = ({ navigation }) => {
   const [selectedId, setSelectedId] = useState('1'); // Default selection set to "Student"
@@ -109,6 +110,7 @@ const LoginScreen = ({ navigation }) => {
         behavior={Platform.OS === 'ios' ? 'padding' : undefined}
       >
         <SafeAreaView style={{ justifyContent: 'center' }}>
+        
           <ScrollView showsVerticalScrollIndicator={false}>
             <LottieView style={styles.image} source={require('../assets/LoginAnimation.json')} autoPlay />
             <Text style={styles.loginText}>Login</Text>
@@ -158,23 +160,12 @@ const LoginScreen = ({ navigation }) => {
                   value={password}
                   onChangeText={setPassword}
                   right={
-                    showPassword ?
-                      (
-                        <TextInput.Icon
-                          icon="eye"
-                          size={24}
-                          style={styles.iconStyle}
-                          onPress={() => setShowPassword(!showPassword)}
-                        />
-                      ):
-                      (
-                        <TextInput.Icon
-                      icon="eye-off"
+                    <TextInput.Icon
+                      icon="eye"
                       size={24}
                       style={styles.iconStyle}
                       onPress={() => setShowPassword(!showPassword)}
                     />
-                      )
                   }
                   left={
                     <TextInput.Icon
