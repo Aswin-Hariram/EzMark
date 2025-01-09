eca1652a3fb8b725a9b69e14ffc7dc96ffb7880f
Aswinhariram-skct <aswincseskct@gmail.com>
2025-01-09 10:43:21 +0530

    Config updated

--- Config/FirebaseConfig.js
+++ Config/FirebaseConfig.js
@@ -7,13 +7,13 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1ofjYOW4AZTJq40dFM5S91XvaPB67aUE",
-  authDomain: "ezmark-e56cc.firebaseapp.com",
-  projectId: "ezmark-e56cc",
-  storageBucket: "ezmark-e56cc.firebasestorage.app",
-  messagingSenderId: "729693697644",
-  appId: "1:729693697644:web:ab4cc2e80f84ef628f8a49",
-  measurementId: "G-RSZEW2JVFY"
+  apiKey: "your-apiKey",
+  authDomain: "your-authDomain",
+  projectId: "your-projectId",
+  storageBucket: "your-storageBucket",
+  messagingSenderId: "your-messagingSenderId",
+  appId: "your-appId",
+  measurementId: "your-measurementId"
 };
 
 // Initialize Firebase
--- Config/awsConfig.js
+++ Config/awsConfig.js
@@ -2,9 +2,9 @@ import AWS from 'aws-sdk';
 
 // Configure AWS with your access keys and region
 AWS.config.update({
-    accessKeyId: 'AKIA23WHTYEBO4PZYR6P',
-    secretAccessKey: 'lI5m42YjErWVejFIZNOIrZ8BQKlMW2zM5/Jokukr',
-    region: 'ap-south-1',
+    accessKeyId: 'your-access-key',
+    secretAccessKey: 'your-secretAccessKey',
+    region: 'your-aws-region',
 });
 
 export const s3 = new AWS.S3();
